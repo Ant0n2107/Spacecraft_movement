1b56fb7f067e46497ca18f68988355b8f6edbf91
Anton <antontertyshnik@mail.ru>
2022-01-13 21:51:38 +0300

    add some staff

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿using System.Windows.Forms;
+
+namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            chartArea2.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.Name = "ChartArea2";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "x";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "y";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "z";
+            series4.ChartArea = "ChartArea2";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Vx";
+            series5.ChartArea = "ChartArea2";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Legend = "Legend1";
+            series5.Name = "Vy";
+            series5.ShadowColor = System.Drawing.Color.Blue;
+            series6.ChartArea = "ChartArea2";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Legend = "Legend1";
+            series6.Name = "Vz";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(950, 661);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 661);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+
+        #endregion
+    }
+}
---
+++ Lab2/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        private static double mu = 3.98603 * Math.Pow(10, 14);
+        private static int days = 86400;
+        private static int h = 5;
+        
+        public struct Vector3
+        {
+            public double X, Y, Z;
+
+            public Vector3(double x, double y, double z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+        }
+
+        public struct VectorX
+        {
+            public Vector3 X;
+
+            public Vector3 V;
+
+            public VectorX(Vector3 x, Vector3 v)
+            {
+                X = x;
+                V = v;
+            }
+        }
+
+        public Vector3 GetA(Vector3 x)
+        {
+            Vector3 A;
+            double r = Math.Sqrt(x.X * x.X + x.Y * x.Y + x.Z * x.Z);
+            A.X = (-mu * x.X) / (r * r * r);
+            A.Y = (-mu * x.Y) / (r * r * r);
+            A.Z = (-mu * x.Z) / (r * r * r);
+            return A;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            List<VectorX> X = new List<VectorX>
+                { new VectorX(new Vector3(42164000, 0, 0), new Vector3(0, 3066, 0)) };
+            for (int t = h; t <= days*2; t += h)
+            {
+                X.Add(Eiler(X[X.Count - 1], h));
+                //X.Add(RungeKutta(X[X.Count - 1], h));
+            }
+
+            int i = 0;
+            for (double t = 0; t <= days*2; t += h)
+            {
+                chart1.Series[0].Points.AddXY(t, X[i].X.X);
+                chart1.Series[1].Points.AddXY(t, X[i].X.Y);
+                chart1.Series[2].Points.AddXY(t, X[i].X.Z);
+                chart1.Series[3].Points.AddXY(t, X[i].V.X);
+                chart1.Series[4].Points.AddXY(t, X[i].V.Y);
+                chart1.Series[5].Points.AddXY(t, X[i].V.Z);
+                i++;
+            }
+        }
+
+        private VectorX Eiler(VectorX x, int h) => new VectorX(
+            new Vector3(x.X.X + h * x.V.X, x.X.Y + h * x.V.Y, x.X.Z + h * x.V.Z),
+            new Vector3(x.V.X + h * GetA(x.X).X, x.V.Y + h * GetA(x.X).Y, x.V.Z + h * GetA(x.X).Z)
+        );
+        
+        VectorX RungeKutta(VectorX X, int h)
+        {
+            Vector3 a = GetA(X.X);
+
+            VectorX k1 = new VectorX(X.V, a);
+
+            VectorX k2 = new VectorX(new Vector3(X.V.X + k1.X.X * h / 2, X.V.Y + k1.X.Y * h / 2, X.V.Z + k1.X.Z * h / 2), 
+                new Vector3(a.X + GetA(k1.X).X * h / 2, a.Y + GetA(k1.X).Y * h / 2, a.Z + GetA(k1.X).Z * h / 2));
+
+            VectorX k3 = new VectorX(new Vector3(X.V.X + k2.X.X * h / 2, X.V.Y + k2.X.Y * h / 2, X.V.Z + k2.X.Z * h / 2),
+                new Vector3(a.X + GetA(k2.X).X * h, a.Y + GetA(k2.X).Y * h, a.Z + GetA(k2.X).Z * h));
+
+            VectorX k4 = new VectorX(new Vector3(X.V.X + k3.X.X * h / 2, X.V.Y + k3.X.Y * h / 2, X.V.Z + k3.X.Z * h / 2),
+                new Vector3(a.X + GetA(k3.X).X * h, a.Y + GetA(k3.X).Y * h, a.Z + GetA(k3.X).Z * h));
+
+            VectorX sum_k = new VectorX(new Vector3(k1.X.X + 2 * (k2.X.X + k3.X.X) + k4.X.X, 
+                k1.X.Y + 2 * (k2.X.Y + k3.X.Y) + k4.X.Y, 
+                k1.X.Z + 2 * (k2.X.Z + k3.X.Z) + k4.X.Z),
+                
+                new Vector3(k1.V.X + 2 * (k2.V.X + k3.V.X) + k4.V.X, 
+                k1.V.Y + 2 * (k2.V.Y + k3.V.Y) + k4.V.Y, 
+                k1.V.Z + 2 * (k2.V.Z + k3.V.Z) + k4.V.Z));
+            
+            return new VectorX(new Vector3(X.X.X + h / 6 * sum_k.X.X, 
+                X.X.Y + h / 6 * sum_k.X.Y, 
+                X.X.Z + h / 6 * sum_k.X.Z),
+                
+                new Vector3(X.V.X + h / 6 * sum_k.V.X, 
+                X.V.Y + h / 6 * sum_k.V.Y, 
+                X.V.Z + h / 6 * sum_k.V.Z));
+        }
+    }
+}
